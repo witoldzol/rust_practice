be94dae03df4ebadf2a75037e86798887642a816
Witold Zolnowski <blue3656@gmail.com>
2021-03-11 14:33:48 +0000

    hash maps

--- src/vectors.rs
+++ src/vectors.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -9,5 +11,34 @@ mod tests {
         nums.push(2);
         let nums2 = vec![1,2];
         assert_eq!(nums, nums2);
+    }
+    
+    #[test]
+    fn access_vector_vals() {
+       let mut vec = vec![1,2,3];
+        assert_eq!(vec[0], 1);
+        println!("{:?}", vec);
+       vec.remove(1);
+        println!("{:?}", vec);
+
+        for x in &mut vec {
+           *x += 1;
+        }
+        println!("{:?}", vec);
+    }
+
+    #[test]
+    fn hash_maps() {
+        let mut map: HashMap<&str, i32> = HashMap::new();
+        map.insert("a", 1);
+        map.insert("b", 2);
+        map.insert("c", 3);
+        println!("{:?}", map);
+
+        for (key, val) in map {
+            // hash-maps do not preserve order !
+            println!("{} => {}", key, val);
+        }
+
     }
 }
